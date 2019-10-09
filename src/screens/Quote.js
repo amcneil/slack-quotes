4414aecea4cb0c67f20683a68a6201171a9bca24
Amanda <amanda.mcneil@fpxus.com>
2019-10-08 19:49:42 -0500

    Add quotes and other details

--- src/screens/Quote.js
+++ src/screens/Quote.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { QuoteBubble, Avatar } from '../components';
+import quotes from '../util/quotes';
+import { random } from 'lodash';
 
 const CloseButton = styled.Text`
-  padding-top: 50px;
+  margin-top: 50px;
+  margin-left: 5px;
   align-self: flex-start;
+  background: #7db1db;
+  border-radius: 5px;
+  padding: 10px;
 `
 const QuoteScreenContainer = styled.View`
   align-items: center;
@@ -13,15 +19,44 @@ const QuoteScreenContainer = styled.View`
 class Quote extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      quotes: [],
+      quote: null,
+      person: {}
+    }
   }
 
-  render() {
+  componentDidMount() {
     const person = this.props.navigation.getParam('person')
+    const personQuotes = quotes[person.name.toLowerCase()]
+    // console.log(personQuotes)
+    this.setState({
+      quotes: personQuotes,
+      person: person,
+    });
+
+  }
+
+  randomQuote(quotes) {
+    const randomQuoteNumber = random(quotes.length)
+    return quotes[randomQuoteNumber]
+  }
+
+  shuffleQuotes() {
+    this.setState({
+      quote: this.randomQuote(this.state.quotes)
+    })
+  }
+  render() {
+    // const quote = this.props.navigation.getParam('quote')
+    // console.log(this.state.quotes)
     return (
       <QuoteScreenContainer>
-        <CloseButton onPress={() => this.props.navigation.navigate('Home')}>Close</CloseButton>
-        <QuoteBubble quote="This is a longer sentence of a test quote from Slack that we might eventually get." />
-        <Avatar person={person} onPress={ () => console.log('reload quote')} />
+        <CloseButton onPress={() => this.props.navigation.navigate('Home')}>Back</CloseButton>
+        {this.state.quote && (
+          <QuoteBubble quote={this.state.quote.text} />
+        )}
+        <Avatar person={this.state.person} onPress={ () => this.shuffleQuotes()} />
       </QuoteScreenContainer>
     );
   }
--- src/util/people.js
+++ src/util/people.js
@@ -1,9 +1,9 @@
 const people = [
-  { name: 'Amanda', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' },
-  { name: 'Gary', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg' },
-  { name: 'Robb', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg' },
-  { name: 'Jared', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg' },
-  { name: 'Mike', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg' },
+  { name: 'Amanda', avatar: 'https://i.ibb.co/YL4Q2vj/Screen-Shot-2019-03-14-at-11-08-38-AM.png' },
+  { name: 'Gary', avatar: 'https://i.ibb.co/qNqvCVT/Screen-Shot-2019-03-14-at-10-42-02-AM.png' },
+  { name: 'Robb', avatar: 'https://i.ibb.co/rFRKk1H/Screen-Shot-2019-03-14-at-10-44-37-AM.png' },
+  { name: 'Jared', avatar: 'https://i.ibb.co/6Rx9Jyt/Screen-Shot-2019-03-14-at-10-43-36-AM.png' },
+  { name: 'Mike', avatar: 'https://i.ibb.co/CmmQT4b/Screen-Shot-2019-03-14-at-10-42-31-AM.png' },
 ];
 
 export default people;
