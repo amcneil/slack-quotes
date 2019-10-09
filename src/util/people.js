179e8901c11dae4e90b375e2a6cd9f73da654a91
Amanda <amanda.mcneil@fpxus.com>
2019-10-08 22:42:38 -0500

    update text

--- src/util/people.js
+++ src/util/people.js
@@ -1,9 +1,9 @@
 const people = [
-  { name: 'Amanda', avatar: 'https://i.ibb.co/YL4Q2vj/Screen-Shot-2019-03-14-at-11-08-38-AM.png' },
-  { name: 'Gary', avatar: 'https://i.ibb.co/qNqvCVT/Screen-Shot-2019-03-14-at-10-42-02-AM.png' },
-  { name: 'Robb', avatar: 'https://i.ibb.co/rFRKk1H/Screen-Shot-2019-03-14-at-10-44-37-AM.png' },
-  { name: 'Jared', avatar: 'https://i.ibb.co/6Rx9Jyt/Screen-Shot-2019-03-14-at-10-43-36-AM.png' },
-  { name: 'Mike', avatar: 'https://i.ibb.co/CmmQT4b/Screen-Shot-2019-03-14-at-10-42-31-AM.png' },
+  { name: 'Alice', avatar: 'https://i.ibb.co/YL4Q2vj/Screen-Shot-2019-03-14-at-11-08-38-AM.png' },
+  { name: 'Geoff', avatar: 'https://i.ibb.co/qNqvCVT/Screen-Shot-2019-03-14-at-10-42-02-AM.png' },
+  { name: 'Ryan', avatar: 'https://i.ibb.co/rFRKk1H/Screen-Shot-2019-03-14-at-10-44-37-AM.png' },
+  { name: 'John', avatar: 'https://i.ibb.co/6Rx9Jyt/Screen-Shot-2019-03-14-at-10-43-36-AM.png' },
+  { name: 'Mark', avatar: 'https://i.ibb.co/CmmQT4b/Screen-Shot-2019-03-14-at-10-42-31-AM.png' },
 ];
 
 export default people;
